62e3f3adb9cbdc1d4547e13254b4e55fcb91f787
victorpavez <73892302+victorpavez@users.noreply.github.com>
2025-04-21 14:26:46 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,21 +1,23 @@
 function enviarFormulario(datos) {
-    fetch("https://script.google.com/macros/s/AKfycby7vv_l_o4IuanKDh-x-H0_IiTb2-kyhTcuqGYwXLR2cByW_8XwV4eX_pqIfP91fhEb/exec", {
+    fetch("https://script.google.com/macros/s/AKfycbzCxbJD6MngF9sZil9ywhZTspK1WVTxEqHCXZFnvMIAAfNs0K2c57KAkos_UqHpzOV2/exec", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(datos)
     })
-      .then(res => {
-        if (res.ok) {
-          document.getElementById("mensaje").innerText = "✅ Trámite enviado correctamente.";
+      .then(response => response.json())
+      .then(data => {
+        if (data.status === "ok") {
+          const mensaje = `✅ Trámite enviado correctamente.\n📄 ID Trámite: ${data.idTramite}`;
+          document.getElementById("mensaje").innerText = mensaje;
           document.getElementById("miFormulario").reset();
         } else {
-          throw new Error("Error en el servidor");
+          throw new Error(data.mensaje || "Error desconocido.");
         }
       })
       .catch(err => {
-        console.error(err);
+        console.error("Error en fetch:", err);
         document.getElementById("mensaje").innerText = "❌ No se pudo enviar el trámite.";
       });
   }
